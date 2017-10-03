1c4d0997f61585a15976509213431a95b5063756
rickdevriess <rickdv@live.com>
2017-10-03 13:56:53 +0200

    Eerste opzet
    Eerste opzet

---
+++ include/menu.php
@@ -0,0 +1,28 @@
+<ul class="nav">  
+    <div class="menuItems">  
+        <li>
+            <a class="centerIcon" href="index" title="Home">
+                <i class="fa fa-home fa-lg" aria-hidden="true"></i><br/>
+                Home
+            </a>
+        </li>
+        <li>
+            <a class="centerIcon" href="nieuws" title="Laatste nieuws">
+                <i class="fa fa-newspaper-o fa-lg" aria-hidden="true"></i><br/>
+                Nieuws
+            </a>
+        </li>
+        <li>
+            <a class="centerIcon" href="bestellen" title="Bestel The Memory Game">
+                <i class="fa fa-shopping-cart fa-lg" aria-hidden="true"></i><br/>
+                Bestellen
+            </a>
+        </li>
+        <li>
+            <a class="centerIcon" href="contact" title="Contact">
+                <i class="fa fa-phone fa-lg" aria-hidden="true"></i><br/>
+                Contact
+            </a>
+        </li>
+    </div>
+</ul>

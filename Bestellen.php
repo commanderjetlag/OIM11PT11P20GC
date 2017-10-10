658ea229bde5edc904e23f6af930037690855d67
Tom van Well <tom_drummen@hotmail.com>
2017-10-10 10:08:15 +0200

    Begin aan bestelpagina
    begin aan bestelpagina

---
+++ Bestellen.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include 'include/head.php'; ?>
+	<title>The Memory Game | Home</title>
+</head>
+<body>
+	<?php include 'include/menu.php'; ?>
+        <div class="wrapperBestellen">
+            <img src="img/logo_memory_game.png" alt = "Memory Game" >
+            <div class="productDescription">
+                <h3>The memory Game</h3>
+                <h1 id="prijs">€64,95</h1>
+            </div>
+    	</div>
+	<footer>
+		Dit is de footer
+	</footer>
+</body>
+<?php include 'include/js.php'; ?>
+</html>
